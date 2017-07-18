f321fb653affeb5899b604e191bb5fb2723a28d1
Sebsatian Murgu <sebastian@airohm.com>
2017-07-17 20:06:25 -0700

    Style Changes Hooked up buttons to dummy input box

--- app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
+++ app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
@@ -1,13 +1,27 @@
 package com.airohm.arithmeticapp;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
+import android.support.annotation.IdRes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText currentInputBox;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        currentInputBox = (EditText) findViewById(R.id.editText);
+    }
+
+    public void numButtonPress(View numButton) {
+        Button b = (Button) numButton;
+        currentInputBox.setText(b.getText());
     }
 }
