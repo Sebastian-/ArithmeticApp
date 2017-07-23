a5a602838e49860289a5e7b1b07d2eeac51762f6
Sebsatian Murgu <sebastian@airohm.com>
2017-07-23 06:59:39 -0700

    Refactored answer array indexing

--- app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
+++ app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
@@ -13,8 +13,6 @@ public class MainActivity extends AppCompatActivity {
 
     TextView currentInputWidget;
     DivisionQuestion currentQuestion;
-    //TODO move answerIndex into question class, make calls to checkInput update accordingly
-    int answerIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,18 +23,16 @@ public class MainActivity extends AppCompatActivity {
         currentQuestion = new DivisionQuestion();
         currentQuestion.renderQuestion(inflater, (ViewGroup) findViewById(R.id.question_area));
         currentQuestion.prepAnswerWidgets(inflater, (ViewGroup) findViewById(R.id.answer_area));
-        currentInputWidget = currentQuestion.getInputWidget(answerIndex);
+        currentInputWidget = currentQuestion.getInputWidget();
     }
 
 
-    //TODO check for out of bounds for answer index
     public void numButtonPress(View numButton) {
         Button b = (Button) numButton;
         String input = b.getText().toString();
-        if (currentQuestion.checkInput(input, answerIndex)) {
+        if (currentQuestion.checkInput(input)) {
             currentInputWidget.setText(input);
-            answerIndex++;
-            currentInputWidget = currentQuestion.getInputWidget(answerIndex);
+            currentInputWidget = currentQuestion.getInputWidget();
         } else {
             currentInputWidget.setText(b.getText());
             currentInputWidget.setTextColor(Color.RED);
