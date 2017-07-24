d6158c35b9267f8c4b4e1f309792d82968b4963a
Sebsatian Murgu <sebastian@airohm.com>
2017-07-23 20:37:49 -0700

    Implemented continuous question generation

--- app/src/main/java/com/airohm/arithmeticapp/DivisionQuestion.java
+++ app/src/main/java/com/airohm/arithmeticapp/DivisionQuestion.java
@@ -13,8 +13,9 @@ class DivisionQuestion {
     private String dividend;
     private String divisor;
     private char[] ans;
-    private DivisionHolder UIholder;
     private int answerIndex;
+    private DivisionHolder UIHolder;
+
 
     // To avoid any GUI issues and make questions manageable, the divisor is limited to 1 digit,
     // and the answer to 3
@@ -39,21 +40,21 @@ class DivisionQuestion {
         holder.question = question;
         holder.num1.setText(dividend);
         holder.num2.setText(divisor);
-        UIholder = holder;
+        UIHolder = holder;
 
         return question;
     }
 
     void prepAnswerWidgets(LayoutInflater inflater, ViewGroup answerArea) {
         for (char c : ans) {
-            TextView answerWidget = (TextView) inflater.inflate(R.layout.answer_widget, answerArea, false);
+            TextView answerWidget = (TextView) inflater.inflate(R.layout.input_widget, answerArea, false);
             answerArea.addView(answerWidget);
-            UIholder.answer.add(answerWidget);
+            UIHolder.answer.add(answerWidget);
         }
     }
 
     TextView getInputWidget() {
-        return UIholder.answer.get(answerIndex);
+        return UIHolder.answer.get(answerIndex);
     }
 
     boolean checkInput(String input) {
@@ -63,7 +64,10 @@ class DivisionQuestion {
         } else {
             return false;
         }
+    }
 
+    boolean isAnswered() {
+        return (answerIndex == ans.length);
     }
 
 }
--- app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
+++ app/src/main/java/com/airohm/arithmeticapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.airohm.arithmeticapp;
 
+import android.content.res.ColorStateList;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,29 +14,45 @@ public class MainActivity extends AppCompatActivity {
 
     TextView currentInputWidget;
     DivisionQuestion currentQuestion;
+    ColorStateList defaultTextColors;
+    ViewGroup questionArea;
+    ViewGroup answerArea;
+    LayoutInflater inflater;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        LayoutInflater inflater = getLayoutInflater();
 
-        currentQuestion = new DivisionQuestion();
-        currentQuestion.renderQuestion(inflater, (ViewGroup) findViewById(R.id.question_area));
-        currentQuestion.prepAnswerWidgets(inflater, (ViewGroup) findViewById(R.id.answer_area));
-        currentInputWidget = currentQuestion.getInputWidget();
+        inflater = getLayoutInflater();
+        questionArea = (ViewGroup) findViewById(R.id.question_area);
+        assembleQuestion();
+        defaultTextColors = currentInputWidget.getTextColors();
     }
-
 
     public void numButtonPress(View numButton) {
         Button b = (Button) numButton;
         String input = b.getText().toString();
         if (currentQuestion.checkInput(input)) {
+            // Input is correct
+            currentInputWidget.setTextColor(defaultTextColors);
             currentInputWidget.setText(input);
+            if (currentQuestion.isAnswered()) {
+                questionArea.removeAllViews();
+                assembleQuestion();
+            }
             currentInputWidget = currentQuestion.getInputWidget();
         } else {
             currentInputWidget.setText(b.getText());
             currentInputWidget.setTextColor(Color.RED);
         }
+    }
+
+    private void assembleQuestion() {
+        currentQuestion = new DivisionQuestion();
+        currentQuestion.renderQuestion(inflater, questionArea);
+        answerArea = (ViewGroup) findViewById(R.id.answer_area);
+        currentQuestion.prepAnswerWidgets(inflater, answerArea);
+        currentInputWidget = currentQuestion.getInputWidget();
     }
 }
